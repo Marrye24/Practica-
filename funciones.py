1438c3ebf6ab273b89226d930c53430b4f7a3b77
Bren Guzmán <125343617+BrenGuzmanB@users.noreply.github.com>
2023-09-11 14:19:38 -0600

    Add files via upload
    Archivo para las funciones

---
+++ funciones.py
@@ -0,0 +1,12 @@
+"""
+Created on Mon Sep 11 14:11:17 2023
+
+@author: Bren Guzmán, María José Merino, Brenda García
+"""
+
+#Distancia Manhattan
+
+#Distancia de Euclidiana
+
+#Distancia Mahalanobis
+
